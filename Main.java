1ee22a3f716c886e53ad693f9b9e58543b7770b5
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-12-09 16:05:08 +0000

    3.3 Notes

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      String danny = "cool";
+      // But Strings are actually OBJECTS 
+      // The original way to create a String: 
+      String steph = new String("GOAT"); 
+      
+      // All objects can be created using this pattern: 
+      // ClassName objectName = new ClassName(); 
+      BuildABear teddyBear = new BuildABear("bear"); 
    }
 }
